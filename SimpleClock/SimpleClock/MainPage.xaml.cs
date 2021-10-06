a49cf4f949cc9737775ee4e19252eaf5ba6910c9
Arttu Höylä <33629266+ajh657@users.noreply.github.com>
2021-10-06 22:01:34 +0300

    Done

---
+++ SimpleClock/SimpleClock/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Timers;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace SimpleClock
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        Timer clockUpdate = new Timer(500);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            clockUpdate.Elapsed += ClockUpdate_Elapsed;
+            clockUpdate.Start();
+        }
+
+        private void ClockUpdate_Elapsed(object sender, ElapsedEventArgs e)
+        {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                ClockTextbox.Text = DateTime.Now.ToString("HH:mm:ss");
+            });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
+
+        
+    }
+}
